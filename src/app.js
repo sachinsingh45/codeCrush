bfa7651ff49cd9bd2ee18977e532826cedb0dee5
sachinsingh45 <sachinsingh16404@gmail.com>
2025-01-28 18:34:05 +0530

    user , request  routes and their API's

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
-// const userRouter = require("./routes/user");
+const userRouter = require("./routes/user");
 // const paymentRouter = require("./routes/payment");
 // const initializeSocket = require("./utils/socket");
 // const chatRouter = require("./routes/chat");
@@ -29,7 +29,7 @@ const requestRouter = require("./routes/request");
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
-// app.use("/", userRouter);
+app.use("/", userRouter);
 // app.use("/", paymentRouter);
 // app.use("/", chatRouter);
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,22 +1,29 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
 const userAuth = async (req, res, next) => {
-    try{
-        const {token} = req.cookies;
-        if(!token){
-            throw new Error("invalid token");
-        }
-        const decodedMessage = jwt.verify(token, "codecrush@123456789");
-        const {_id} = decodedMessage;
-        const user = await User.findById(_id);
-        if(!user){
-            throw new Error("User not found");
-        }
-        req.user = user;
-        next();
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please Login!");
     }
-    catch(err){
-        res.status(400).send("ERROR: "+ err.message);
+
+    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
     }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 };
-module.exports = {userAuth,}
+
+module.exports = {
+  userAuth,
+};
