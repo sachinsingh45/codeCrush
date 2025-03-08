ca0d181f8dd11b99644b23b4aa91e37b1c2d3021
sachinsingh45 <sachinsingh16404@gmail.com>
2025-03-08 19:50:45 +0530

    dotenv

--- src/app.js
+++ src/app.js
@@ -29,7 +29,6 @@ app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
-// app.use("/", paymentRouter);
 app.use("/", chatRouter);
 
 const server = http.createServer(app);
