5c5e3ac79406992cbff55bfbb588b26345b10870
sachinsingh45 <sachinsingh16404@gmail.com>
2025-03-30 18:47:23 +0530

    build chat

--- src/app.js
+++ src/app.js
@@ -3,48 +3,41 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-app.use(express.json());
-app.use(cookieParser());
+const http = require("http");
 
 require("dotenv").config();
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"], // Ensure OPTIONS is included
-    allowedHeaders: ["Content-Type", "Authorization"], // Explicitly allow headers
   })
 );
+app.use(express.json());
+app.use(cookieParser());
 
-// Explicitly handle OPTIONS requests for CORS preflight
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  return res.sendStatus(200);
-});
-
-// Importing routes
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
   .then(() => {
     console.log("Database connection established...");
-    const port = 3000;
-    app.listen(port, () => {
-      // ✅ Use backticks (`) for variable interpolation
-      console.log(`Server is successfully listening on port ${port}...`);
+    server.listen(process.env.PORT, () => {
+      console.log("Server is successfully listening on port 7777...");
     });
   })
   .catch((err) => {
-    console.error("Database cannot be connected!!" , err);
+    console.error("Database cannot be connected!!");
   });
